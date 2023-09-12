03e69d215140bb551815d8d2a619d4e113871ed7
Hannah Nguyen <hnguyen160801@gmail.com>
2023-09-12 19:25:44 -0400

    update font

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -14,23 +14,23 @@ function NavBar({ logout }: NavBarPropsInterface) {
 
     function loggedOutNav() {
         return (<>
-            <NavLink className="nav-link" to={"/login"}>Log In</NavLink>
-            <NavLink className="nav-link" to={"/register"}>Register</NavLink>
+            <NavLink className="nav-link main-font" to={"/login"}>Log In</NavLink>
+            <NavLink className="nav-link main-font" to={"/register"}>Register</NavLink>
         </>);
     }
 
     function loggedIn() {
         return (<>
-            <NavLink className="nav-link" to={"/profile"}>Profile</NavLink>
-            <NavLink className="nav-link" to={"/matching"}>Matching</NavLink>
-            <NavLink className="nav-link" to={"/friends"}>Friends</NavLink>
-            <NavLink className="nav-item nav-link" to="/" onClick={logout}>Log Out</NavLink>
+            <NavLink className="nav-link main-font" to={"/profile"}>Profile</NavLink>
+            <NavLink className="nav-link main-font" to={"/matching"}>Matching</NavLink>
+            <NavLink className="nav-link main-font" to={"/friends"}>Friends</NavLink>
+            <NavLink className="nav-item nav-link main-font" to="/" onClick={logout}>Log Out</NavLink>
         </>);
     }
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between p-2">
-            <NavLink className="nav-link mx-1 fw-bold" to={'/'}>
+            <NavLink className="nav-link mx-1 fw-bold main-font" to={'/'}>
                 <img className="w-25 me-2"src="/images/smiling_cowboy.png" alt="smiling cowboy emoji"/>
                 Howdy
             </NavLink>
